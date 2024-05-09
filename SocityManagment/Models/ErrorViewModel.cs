d4bdf30692614b720de6a822fe57c75eca4db013
Himanshu Kumar <hkumar@vohraphysicians.com>
2024-05-09 09:54:28 +0530

    Add project files.

---
+++ SocityManagment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocityManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
